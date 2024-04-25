2a0892cbd2f8b1374bdedac6df73e9fc01dd229a
Favien Remond <f.remond@unistra.fr>
2024-04-25 17:12:09 +0200

    ajout de la classe Pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,7 +1,37 @@
+import java.util.Random;
 public class Pokemon
 {
-  public static void main(String args[])
-  {
-    System.out.println("Lancement du jeu ...");
+  private String m_nom;
+  private int m_pvMax;
+  private int m_pv;
+  private int m_attaque;
+  private int m_element;
+  public Pokemon(){
+    Random random = new Random();
+    int randomNumber = random.nextInt(0) + m_nomMaxPokemon;
+    m_nom = m_tabPokemon.get(randomNumber);
+    m_nomMaxPokemon.remove(randomNumber);
+    m_nomMaxPokemon -=1;
+    randomNumber = random.nextInt(11) + 10;
+    m_pvMax = randomNumber *= 10;
+    m_pv = m_pvMax;
+    randomNumber = random.nextInt(0) + 4;
+    m_attaque = randomNumber *= 10;
+    randomNumber = random.nextInt(1) + 4;
+    m_element = randomNumber;
+  }
+  public void attaquer(Pokemon pokemon){
+    pokemon.subirDegats(this.m_attaque+=m_tabElt[pokemon.m_element,this.m_element]);
+  }
+  public void subirDegats(int degats){
+    this.m_pv -= degats;
+  }
+  public Boolean estVivant(){
+    if(this.m_pv<=0){
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 }
