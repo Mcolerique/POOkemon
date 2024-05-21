8c874f74cfeaff82061da0aff65900eaa5d41790
HIEBER Gaétan <gaetan.hieber@etu.unistra.fr>
2024-05-21 11:34:21 +0200

    creation de Pouvoir.java

---
+++ src/Pouvoir.java
@@ -0,0 +1,12 @@
+public abstract class Pouvoir {
+    private String m_nom;
+    private boolean m_utilisableChaqueTour;
+    private boolean m_utilise;
+    private String m_desc;
+
+    public Pouvoir(String nom, boolean utilisableChaqueTour) {
+        this.m_nom = nom;
+        this.m_utilisableChaqueTour = utilisableChaqueTour;
+        this.m_utilise = false;
+    }
+}
