8bcc930c739540ba3cd737934177f7e1993271d2
HIEBER Gaétan <gaetan.hieber@etu.unistra.fr>
2024-05-22 14:41:09 +0200

    creation de ExtensionTerritoire dans Pouvoir

---
+++ src/pokemons/pouvoirs/ExtensionTerritoire.java
@@ -0,0 +1,7 @@
+package pokemons.pouvoirs;
+
+public class ExtensionTerritoire extends Pouvoir{
+    public ExtensionTerritoire(String nom, boolean utilisableChaqueTour) {
+        super(nom, utilisableChaqueTour);
+    }
+}
