57ee7d3ff3df42aad8235dfc5a2e6181d3794304
Deborah Ferreira <ferreiralourenco@unistra.fr>
2024-06-09 22:25:15 +0200

    Ecriture de la javadoc dans la classe Start

--- src/jeu/Start.java
+++ src/jeu/Start.java
@@ -1,7 +1,15 @@
 package jeu;
 
+/**
+ * La classe Start contient la méthode main pour lancer le jeu Pokémon.
+ */
 public class Start {
-    //Classe qui contient la méthode main pour lancer le jeu
+
+    /**
+     * La méthode main pour lancer le jeu.
+     *
+     * @param args Les arguments de la ligne de commande.
+     */
     public static void main(String[] args) {
         Jeu jeu = new Jeu();
         jeu.initialisationJeu();
